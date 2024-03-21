f2c0a5b31d96ed05043f3d67005fb82474d60401
Dulguun Batbaatar <23LP7194@G304-CC0064.local>
2024-03-21 18:09:18 +0800

    create travel sections 1-3

---
+++ src/components/admin/Leftbar.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { AdminBoard } from "../icons/adminicons/AdminBoard";
+import { AdminOrder } from "../icons/adminicons/AdminOrder";
+import { AdminRevenue } from "../icons/adminicons/AdminRevenue";
+import { AdminSetting } from "../icons/adminicons/AdminSetting";
+import { AdminProduct } from "../icons/adminicons/AdminProduct";
+
+export const Leftbar = () => {
+  return (
+    <div className="w-1/6 min-h-screen h-full py-10 flex flex-col gap-7 justify-start bg-white shadow-lg">
+      <a className="flex gap-2 w-full items-center pl-5" href="/">
+        <div className="w-1/6 px-4">
+          <AdminBoard />
+        </div>
+        <h1>Хяналтын самбар</h1>
+      </a>
+      <a className="flex gap-2 w-full items-center pl-5" href="/">
+        <div className="w-1/6 px-4">
+          <AdminOrder />
+        </div>
+        <h1>Захиалга</h1>
+      </a>
+      <a className="flex gap-2 w-full items-center pl-5" href="/">
+        <div className="w-1/6 px-4">
+          <AdminRevenue />
+        </div>
+        <h1>Орлого</h1>
+      </a>
+      <a className="flex gap-2 w-full items-center pl-5" href="/">
+        <div className="w-1/6 px-4">
+          <AdminProduct />
+        </div>
+        <h1>Бүтээгдэхүүн</h1>
+      </a>
+      <a className="flex gap-2 w-full items-center pl-5" href="/">
+        <div className="w-1/6 px-4">
+          <AdminSetting />
+        </div>
+        <h1>Тохиргоо</h1>
+      </a>
+    </div>
+  );
+};
---
+++ src/pages/travelinputone.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Leftbar } from "@/components/admin/Leftbar";
+import { TravelAdminOne } from "@/components/travelinput/TravelAdminOne";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Travelinputone() {
+  return (
+    <main
+      className={`flex min-h-screen h-full w-full bg-white justify-start items-start ${inter.className}`}
+    >
+      <Leftbar />
+      <div className="flex w-full min-h-screen h-full bg-gray-100">
+        <TravelAdminOne />
+      </div>
+    </main>
+  );
+}
