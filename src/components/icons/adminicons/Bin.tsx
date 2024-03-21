d0fc1f524fc4abbb4b496d1cda2ee857f298c7ea
Dulguun Batbaatar <23LP7194@G304-CC0064.local>
2024-03-21 18:09:18 +0800

    create travel sections 1-3

---
+++ src/components/icons/adminicons/Bin.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Bin = () => {
+  return (
+    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21.3334 15.5V23.8333H14.6667V15.5H21.3334ZM20.0834 10.5H15.9167L15.0834 11.3333H12.1667V13H23.8334V11.3333H20.9167L20.0834 10.5ZM23 13.8333H13V23.8333C13 24.75 13.75 25.5 14.6667 25.5H21.3334C22.25 25.5 23 24.75 23 23.8333V13.8333Z" fill="#1C2024" fill-opacity="0.24"/>
+</svg>
+
+  )
+}
+
