cd20582d6aaf074c250901ec4b4bc7aafa518396
Baatar Chuluunbat <23LP0037@G304-CC0084.local>
2024-03-22 19:26:01 +0800

    step four five

---
+++ src/components/icons2/product/Deleteicon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Deleteicon = () => {
+  return (
+    <div>
+      <svg
+        width="15"
+        height="20"
+        viewBox="0 0 12 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M9.33268 5.5V13.8333H2.66602V5.5H9.33268ZM8.08268 0.5H3.91602L3.08268 1.33333H0.166016V3H11.8327V1.33333H8.91602L8.08268 0.5ZM10.9993 3.83333H0.999349V13.8333C0.999349 14.75 1.74935 15.5 2.66602 15.5H9.33268C10.2493 15.5 10.9993 14.75 10.9993 13.8333V3.83333Z"
+          fill="#1C2024"
+          fill-opacity="0.24"
+        />
+      </svg>
+    </div>
+  );
+};
