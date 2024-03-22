67e1e3000e476f91099863eb67175b972a4556e8
Baatar Chuluunbat <23LP0037@G304-CC0084.local>
2024-03-22 18:49:48 +0800

    step four five

---
+++ src/components/admin/AdminDashbourd.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { Navbar } from "./nav/Navbar";
+import { Bagaih } from "../icons2/product/Bagaih";
+import { Ordericon } from "../icons2/Ordericon";
+
+export const AdminDashbourd = () => {
+  const data = [{}, {}, {}, {}, {}];
+  return (
+    <div className="flex">
+      <Navbar />
+      <div className="bg-gray-200 w-full grid grid-cols-8 gap-x-8 p-8">
+        <div className=" w-full col-span-4 grid grid-rows-5 gap-8">
+          <div className=" rounded-lg p-3 border-none bg-white row-span-1">
+            <h3>$ Орлого</h3>
+            <h1 className=" text-2xl font-bold">235,000₮</h1>
+            <p className=" text-[]">Өнөөдөр</p>
+          </div>
+          <div className=" rounded-lg p-3 border-none bg-white row-span-4">
+            <div className=" flex justify-between mb-3">
+              <h1 className=" text-xl">Шилдэг бүтээгдэхүүн</h1>
+              <Bagaih />
+            </div>
+            <table className="table bg-white">
+              {/* head */}
+              <thead className=" bg-gray-200">
+                <tr>
+                  <th>
+                    <label>№</label>
+                  </th>
+                  <th>Бүтээгдэхүүн</th>
+                  <th>Зарагдсан</th>
+                  <th>Үнэ</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map(() => {
+                  return (
+                    <tr>
+                      <th>
+                        <label>
+                          <input type="checkbox" className="checkbox" />
+                        </label>
+                      </th>
+                      <td>
+                        <div className="flex items-center gap-3">
+                          <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                              <img
+                                src="/tailwind-css-component-profile-2@56w.png"
+                                alt="Avatar Tailwind CSS Component"
+                              />
+                            </div>
+                          </div>
+                          <div>
+                            <div className="font-bold">Hart Hagerty</div>
+                            <div className="text-sm opacity-50">
+                              United States
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p>Эмэгтэй, цүнх</p>
+                      </td>
+                      <td>19,000$</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className=" w-full col-span-4 grid grid-rows-5 gap-8">
+          <div className=" rounded-lg p-3 border-none bg-white row-span-1">
+            <h2 className=" flex">
+              <Ordericon /> Захиалга
+            </h2>
+            <h1 className=" text-2xl font-bold">58</h1>
+            <p className=" text-[]">Өнөөдөр</p>
+          </div>
+          <div className=" rounded-lg p-3 border-none bg-white row-span-2">
+            <div className=" flex justify-between mb-3">
+              <h1 className=" text-xl">Борлуулалт</h1>
+              <Bagaih />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/travelInput2/TravelCalendar.tsx
@@ -0,0 +1,154 @@
+import React, { ReactNode, useState } from "react";
+import { Deleteicon } from "../icons2/product/Deleteicon";
+
+type rowDataProps = {
+  startTime: string;
+  startDay: string;
+  endTime: string;
+  endDay: string;
+};
+
+export const TravelCalendar = () => {
+  const [travelInput, setTravelInput] = useState({
+    startTimes: "",
+    endTime: "",
+    startDay: "",
+    endDay: "",
+  });
+  const [travelData, setTravelData] = useState<rowDataProps[]>([]);
+
+  const add = async () => {
+    const travelInputValue: rowDataProps = {
+      startTime: travelInput.startTimes,
+      startDay: travelInput.startDay,
+      endTime: travelInput.endTime,
+      endDay: travelInput.endDay,
+    };
+    setTravelData([...travelData, travelInputValue]);
+    clear();
+  };
+  const clear = () => {
+    travelInput.startTimes = "";
+    travelInput.endTime = "";
+    travelInput.startDay = "";
+    travelInput.endDay = "";
+    setTravelInput({ ...travelInput, endDay: "" });
+  };
+  const deleteRows = (id: number) => {
+    const rowValues = travelData.filter(
+      (travelCalendar, index) => index !== id
+    );
+    setTravelData(rowValues);
+  };
+  return (
+    <div>
+      <ul className="w-full justify-center steps steps-vertical lg:steps-horizontal py-7">
+        <li className="step step-primary">General information</li>
+        <li className="step step-primary">Food</li>
+        <li className="step step-primary">Traffic</li>
+        <li className="step step-primary">Routes</li>
+        <li className="step step-primary">Calendar</li>
+      </ul>
+      ;
+      <div className=" flex bg-gray-100 p-10 gap-10">
+        <div className=" border-none p-5 w-3/12 rounded flex flex-col bg-gray-200 h-fit">
+          <div className=" flex justify-between mb-3">
+            <h1 className=" text-xl font-normal">TravelCalendar</h1>
+            <button onClick={clear} className="">
+              <Deleteicon />
+            </button>
+          </div>
+          <input
+            type="time"
+            value={travelInput.startTimes}
+            className=" border border-gray-300 rounded-lg mb-2 p-1 w-full"
+            onChange={(e) => {
+              setTravelInput({
+                ...travelInput,
+                startTimes: e.target.value,
+              });
+            }}
+          />
+          <input
+            type="time"
+            placeholder="Дуусах цаг"
+            value={travelInput.endTime}
+            className=" border border-gray-300 rounded-lg mb-2 p-1 w-full"
+            onChange={(e) => {
+              setTravelInput({ ...travelInput, endTime: e.target.value });
+            }}
+          />
+          <input
+            type="date"
+            placeholder="Эхлэх өдөр"
+            className=" border border-gray-300 rounded-lg mb-2 p-1 w-full"
+            value={travelInput.startDay}
+            onChange={(e) => {
+              setTravelInput({
+                ...travelInput,
+                startDay: e.target.value,
+              });
+            }}
+          />
+          <input
+            type="date"
+            placeholder="Дуусах өдөр"
+            value={travelInput.endDay}
+            className=" border border-gray-300 rounded-lg mb-2 p-1 w-full"
+            onChange={(e) => {
+              setTravelInput({ ...travelInput, endDay: e.target.value });
+            }}
+          />
+          <div className=" flex justify-center text-xl mt-3">
+            <button
+              onClick={add}
+              className=" bg-slate-200 p-2 rounded-lg"
+              disabled={
+                !travelInput.endDay ||
+                !travelInput.endTime ||
+                !travelInput.startDay ||
+                !travelInput.startTimes
+              }
+            >
+              Add Rows
+            </button>
+          </div>
+        </div>
+        <div className=" w-9/12 ">
+          <table className="table bg-white">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Эхлэх цаг</th>
+                <th>Дуусах цаг</th>
+                <th>Эхлэх өдөр</th>
+                <th>Дуусах өдөр</th>
+                <th>delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {travelData.map((travelCalendar: rowDataProps, index: number) => {
+                return (
+                  <tr>
+                    <th>{index + 1}</th>
+                    <td>{travelCalendar?.startTime}</td>
+                    <td>{travelCalendar?.endTime}</td>
+                    <td>{travelCalendar?.startDay}</td>
+                    <td>{travelCalendar?.endDay}</td>
+                    <td>
+                      <button onClick={() => deleteRows(index)}>
+                        <Deleteicon />
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
