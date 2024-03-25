3f347468bbdfed8512926481c087123f419d2610
syerikkh <147494171+syerikkh@users.noreply.github.com>
2024-03-25 19:31:12 +0800

    2 home page (#14)
    * do header part
    
    * do the hero
    
    ..
    
    * do Header
    
    * do headers
    
    * 1.do hero page 2.popular destinatons 3.on process
    
    * do process part
    
    process
    
    * last part left
    
    * home page
    
    do the home page
    
    * do hover
    
    * home
    
    home
    
    * home
    
    home
    
    * do home page
    
    ---------
    
    Co-authored-by: 94241021 Khuanysh <23LP2243@G304-CC0080.local>

---
+++ src/components/Home/CarouselCard.tsx
@@ -0,0 +1,44 @@
+import React, { ReactElement, useState } from 'react'
+
+interface Props {
+  image: string
+  title: string
+  icon: ReactElement
+  miniTitle: string
+  description: string
+}
+
+export const CarouselCard = ({ image, title, icon, miniTitle, description }: Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+
+    <div className='relative'>
+      <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className='border cursor-pointer rounded-3xl'>
+        <img src={image} className={`${isHovered ? "w-[713px] h-[609px]" : "w-[372px] h-[503px]"} transition-all ease-out duration-200 rounded-3xl`} />
+        {isHovered
+          ? <div className='absolute top-0 w-full h-full'>
+            <div className="bg-gradient-to-t from-black to-transparent absolute inset-0 rounded-3xl">
+              <div className='absolute bottom-0'>
+                <div className='p-8 flex font-primary flex-col gap-3 text-white'>
+                  <h1 className='text-3xl font-medium'>{title}</h1>
+                  <div className='flex gap-2 items-center'>
+                    {icon}
+                    <p className='text-xl font-normal'>{miniTitle}</p>
+                  </div>
+                  <p className='text-xl text-gray-500'>{description}</p>
+                  <div className='flex gap-5'>
+                    <button className='p-3  w-[147px] rounded-lg font-medium bg-white text-black'>Book now</button>
+                    <button className='border w-[147px] rounded-lg font-medium border-white p-3'>Learn more</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          : <div></div>
+        }
+      </div>
+    </div>
+
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,34 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+import { Process } from "@/components/Home/Process";
+import { Value } from "@/components/Home/Value";
+import { Questions } from "@/components/Home/Questions";
+import { MainFooter } from "@/components/Home/MainFooter";
+import { Data } from "@/components/Home/types/heroTypes";
+import { PopularDestinations } from "@/components/Home/PopularDestinations";
+import { getServerSideProps } from "@/components/Home/heroData";
+import Hero from "@/components/Home/Hero";
+import Gallery from "@/components/Home/Gallery";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin'],
+})
 
-export default function Home() {
+function Home({ data }: { data: Data[] }) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    ></main>
+    <div>
+      <Hero data={data} />
+      <PopularDestinations data={data} />
+      <Process />
+      <Gallery data={data} />
+      <Value />
+      <Questions />
+      <MainFooter />
+    </div>
   );
 }
+
+export { getServerSideProps };
+
+export default Home;
