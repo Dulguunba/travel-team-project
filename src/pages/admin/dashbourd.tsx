6cd81fd7aba36742570c63defa8b7a3abdfa60ad
Baatar Chuluunbat <23LP0037@G304-CC0084.local>
2024-03-28 19:35:27 +0800

    admin dashboard

---
+++ src/pages/admin/dashbourd.tsx
@@ -0,0 +1,10 @@
+import { OrderA } from "@/components/admin2/order/OrderA";
+import React from "react";
+
+export default function dashbourd() {
+  return (
+    <div>
+      <OrderA />
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Orderdetail } from "@/components/admin2/order/Orderdetail";
+import { OrderCard } from "@/components/admin2/order/OrderCard";
+import { Leftbar } from "@/components/admin/Leftbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    ></main>
+    <main>
+      <Orderdetail />
+      {/* <Leftbar /> */}
+    </main>
   );
 }
