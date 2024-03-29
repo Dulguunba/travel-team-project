d444ec7d24441472d54af0f70cb44ad5dcc07ad7
syerikkh <147494171+syerikkh@users.noreply.github.com>
2024-03-29 19:37:10 +0800

    17 syerik detail (#23)
    * start detail page
    
    start detail page
    
    * do detail page
    
    * home
    
    home
    
    * home
    
    home
    
    * do detail page
    
    * do pagination
    
    do pagination and stuck on carousel
    
    * do carousel
    
    * do carousel
    
    carousel
    
    * fetch data backend
    
    ---------
    
    Co-authored-by: 94241021 Khuanysh <23LP2243@G304-CC0080.local>

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,10 +1,27 @@
 import React, { useState } from 'react'
 import { DownArrow } from '../icons/HomeIcons/DownArrow'
 import { Data } from './types/heroTypes'
-import { getServerSideProps } from './heroData';
+// import { getServerSideProps } from './heroData';
 import Link from 'next/link';
+import { GetServerSideProps } from 'next';
+import axios from 'axios';
 
-const Header = ({ data }: { data: Data[] }) => {
+export interface Tours {
+    result: [{
+        _id: string
+        name: string
+        english: string
+        group: string
+    }]
+}
+
+const Header = ({ toursData }: { toursData: Tours }) => {
+    const tours = toursData.result
+    console.log('tours', tours)
+
+    const personalTours = tours.filter(tour => tour.group === "personal tours");
+    const groupTours = tours.filter(tour => tour.group === "groups tour");
+
     return (
         <div className="flex flex-col items-center justify-center">
             <div className='flex max-w-[1520px] w-[90%] z-10'>
@@ -18,19 +35,27 @@ const Header = ({ data }: { data: Data[] }) => {
                                 <Link href={"/destination"}><div tabIndex={0} role="button" className="m-1 flex gap-2 items-center">Destination <DownArrow /></div></Link>
                                 <ul tabIndex={0} className="dropdown-content -left-80 z-[1] menu p-2 shadow bg-base-100 rounded-box w-[60vw] flex-row grid grid-cols-5">
                                     <div className="text-gray-700 font-semibold border-b-2 w-full pb-2 col-span-5">Popular Destinations</div>
-                                    {data.map((item) => (
+                                    {/* {data.map((item) => (
                                         <li className='text-black'>
                                             <a>{item.title}</a>
                                         </li>
-                                    ))}
+                                    ))} */}
                                 </ul>
                             </div>
                         </div>
                         <div className='flex items-center gap-2 cursor-pointer hover:-translate-y-1 transition ease-in-out'>
                             <div className="dropdown dropdown-hover relative">
                                 <div tabIndex={0} role="button" className="m-1 flex gap-2 items-center">Tours <DownArrow /></div>
-                                <ul tabIndex={0} className="dropdown-content -left-80 z-50 menu p-2 shadow w-[60vw] bg-base-100 rounded-box flex-row grid grid-cols-5">
-                                    {data?.map((item) => <li className='text-black'><a>{item.title}</a></li>)}
+                                <ul tabIndex={0} className="dropdown-content z-50 menu p-2 shadow bg-base-100 w-[400px] rounded-box flex-row grid grid-cols-2">
+
+                                    <div className="text-gray-700 font-semibold border-b-2  text-center capitalize">
+                                        {personalTours.length > 0 ? personalTours[0].group : ''}
+                                    </div>
+                                    <div className="text-gray-700 font-semibold border-b-2 text-center capitalize">
+                                        {groupTours.length > 0 ? groupTours[0].group : ''}
+                                    </div>
+
+
                                 </ul>
                             </div>
                         </div>
@@ -47,6 +72,25 @@ const Header = ({ data }: { data: Data[] }) => {
     )
 };
 
-export { getServerSideProps }
+// export { getServerSideProps }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    try {
+        const res = await axios.get("http://localhost:8800/tourist/get");
+        const toursData: Tours = res.data;
+        return {
+            props: {
+                toursData
+            }
+        }
+    } catch (error) {
+        console.error("Error fetching data:hasdhaskjdhaskdhjkas", error);
+        return {
+            props: {
+                toursData: []
+            }
+        };
+    }
+}
 
 export default Header
