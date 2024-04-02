df554a2a4c5b8d54edcae5e6013bbdae2667e13d
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2024-04-02 17:06:33 +0800

    last comment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ import { Process } from "@/components/Home/Process";
 import { Value } from "@/components/Home/Value";
 import { Questions } from "@/components/Home/Questions";
 import { MainFooter } from "@/components/Home/MainFooter";
-
 import Hero, { getServerSideProps } from "@/components/Home/Hero";
 import Gallery from "@/components/Home/Gallery";
 import PopularDestinations from "@/components/Home/PopularDestinations";
 import { Tours } from "@/types/toursTypes";
 import { Travel } from "@/types/travelTypes";
+
 interface Props {
   toursData: Tours
   travelDatas: Travel
